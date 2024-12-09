71d7c2d58dc0fa3cb9e4c6c442f29b5d68b6e801
teacher <tibedox@mail.ru>
2024-12-09 14:27:09 +0600

    Start

---
+++ core/src/main/java/ru/itschool/newyear/Main.java
@@ -0,0 +1,53 @@
+package ru.itschool.newyear;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+    public static final float SCR_WIDTH = 1600;
+    public static final float SCR_HEIGHT = 900;
+
+    private SpriteBatch batch;
+    private OrthographicCamera camera;
+    private Vector3 touch;
+    private Texture image;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, SCR_WIDTH, SCR_HEIGHT);
+        touch = new Vector3();
+        image = new Texture("libgdx.png");
+    }
+
+    @Override
+    public void render() {
+        // касания
+        if(Gdx.input.justTouched()){
+            touch.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            camera.unproject(touch);
+        }
+        // события
+
+        // отрисовка
+        batch.setProjectionMatrix(camera.combined);
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        batch.begin();
+        batch.draw(image, 140, 210);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        image.dispose();
+    }
+}
