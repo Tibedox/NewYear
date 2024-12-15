9fe9edccb5f384283ed0d7d61e283a385468dbea
Samsung120 <Samsung@sch120.ru>
2024-12-15 12:44:12 +0600

    keyboard

--- core/src/main/java/ru/itschool/newyear/Main.java
+++ core/src/main/java/ru/itschool/newyear/Main.java
@@ -2,9 +2,10 @@ package ru.itschool.newyear;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -17,7 +18,17 @@ public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private OrthographicCamera camera;
     private Vector3 touch;
-    private Texture image;
+
+    private BitmapFont font;
+    private InputKeyboard keyboard;
+    boolean isKeyboardShow;
+    String name;
+
+    private Texture imgBackGround;
+    private Texture imgFlake;
+    private Music sndElochka1, sndElochka2;
+
+    SnowFlake[] flakes = new SnowFlake[200];
 
     @Override
     public void create() {
@@ -25,7 +36,18 @@ public class Main extends ApplicationAdapter {
         camera = new OrthographicCamera();
         camera.setToOrtho(false, SCR_WIDTH, SCR_HEIGHT);
         touch = new Vector3();
-        image = new Texture("libgdx.png");
+
+        font = new BitmapFont(Gdx.files.internal("stylo55.fnt"));
+        keyboard = new InputKeyboard(font, SCR_WIDTH, SCR_HEIGHT, 12);
+
+        imgBackGround = new Texture("winterimg.jpg");
+        imgFlake = new Texture("snowflake.png");
+        sndElochka1 = Gdx.audio.newMusic(Gdx.files.internal("v-lesu-rodilas-elochka1.mp3"));
+        sndElochka2 = Gdx.audio.newMusic(Gdx.files.internal("v-lesu-rodilas-elochka2.mp3"));
+
+        for (int i = 0; i < flakes.length; i++) {
+            flakes[i] = new SnowFlake();
+        }
     }
 
     @Override
@@ -34,20 +56,45 @@ public class Main extends ApplicationAdapter {
         if(Gdx.input.justTouched()){
             touch.set(Gdx.input.getX(), Gdx.input.getY(), 0);
             camera.unproject(touch);
+
+            for(SnowFlake f: flakes){
+                if(f.hit(touch.x, touch.y)) f.respawn();
+            }
+
+            if(keyboard.endOfEdit(touch.x, touch.y)) name = keyboard.getText();
+
+            if(871<touch.x && touch.x<940 && 308<touch.y && touch.y<373){
+                sndElochka2.stop();
+                sndElochka1.play();
+                keyboard.start();
+            }
+            if(745<touch.x && touch.x<810 && 340<touch.y && touch.y<402){
+                sndElochka1.stop();
+                sndElochka2.play();
+            }
         }
         // события
+        for (SnowFlake f: flakes) f.fly();
 
         // отрисовка
         batch.setProjectionMatrix(camera.combined);
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(imgBackGround, 0, 0, SCR_WIDTH, SCR_HEIGHT);
+        for (SnowFlake f: flakes) {
+            batch.draw(imgFlake, f.x, f.y, f.width/2, f.height/2, f.width, f.height, 1, 1, f.rotation, 0, 0, imgFlake.getWidth(), imgFlake.getHeight(), false, false);
+        }
+        keyboard.draw(batch);
         batch.end();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        imgBackGround.dispose();
+        imgFlake.dispose();
+        sndElochka1.dispose();
+        sndElochka2.dispose();
+        keyboard.dispose();
     }
 }
---
+++ core/src/main/java/ru/itschool/newyear/SnowFlake.java
@@ -0,0 +1,41 @@
+package ru.itschool.newyear;
+
+import static ru.itschool.newyear.Main.*;
+
+import com.badlogic.gdx.math.MathUtils;
+
+public class SnowFlake {
+    public float x;
+    public float y;
+    public float width;
+    public float height;
+    float stepX;
+    float stepY;
+    float rotation;
+    float speedRotation;
+
+    public SnowFlake(){
+        respawn();
+        this.y = MathUtils.random(0, SCR_HEIGHT);
+        speedRotation = MathUtils.random(-0.1f, 0.1f);
+    }
+
+    public void fly(){
+        x += stepX;
+        y += stepY;
+        if (y < - height) respawn();
+        rotation += speedRotation;
+    }
+
+    void respawn(){
+        width = height = MathUtils.random(10, 30);
+        this.x = MathUtils.random(0, SCR_WIDTH);
+        this.y = MathUtils.random(SCR_HEIGHT+height, SCR_HEIGHT*2);
+        stepX = MathUtils.random(-0.2f, 0.2f);
+        stepY = MathUtils.random(-2f, -1);
+    }
+
+    boolean hit(float tx, float ty){
+        return x<tx && tx<x+width && y<ty && ty<y+height;
+    }
+}
